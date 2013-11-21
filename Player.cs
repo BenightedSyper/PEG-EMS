3cc61c18b571afe60f75612e34f85ef3065ede3c
BenightedSyper <devek.paid@gmail.com>
2013-11-21 09:31:17 -0700

    starting file

---
+++ Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public int Team;
+	public List<Card> Hand;
+	public List<Card> Discard;
+	public Board MyBoard;
+	
+	void Start () {
+	    
+	}
+	
+	void Update () {
+	
+	}
+}
