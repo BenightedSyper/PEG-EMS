16605f2398f3aaea351b4e9b3b02fc0e2846991e
BenightedSyper <devek.paid@gmail.com>
2013-11-24 19:39:02 -0700

    changes to GUI logic and discard

--- Table.cs
+++ Table.cs
@@ -11,8 +11,10 @@ public class Table : MonoBehaviour {
 	public float CARDHEIGHT = 50f;
 	public float CARDBUFFER = 10f;
 	
+	public Board TableBoard;
 	public Deck TableDeck;
 	public List<Player> Players;
+	public Card LastPlayed;
 	
 	private RULESET RuleSet;
 	private int TurnCounter;
@@ -37,7 +39,7 @@ public class Table : MonoBehaviour {
 	
 	void OnGUI(){
 		if(Players.Count > 0){
-			foreach(Player _player in Players){
+			/*foreach(Player _player in Players){
 				if(_player.Hand.Count > 0){
 					GUILayout.BeginHorizontal("");
 					foreach(Card _card in _player.Hand){
@@ -45,16 +47,45 @@ public class Table : MonoBehaviour {
 							_player.SelectedCard = _player.Hand.IndexOf(_card);
 						}
 					}
-					if(GUI.Button(new Rect((CARDWIDTH + CARDBUFFER)* _player.Hand.Count, (CARDHEIGHT + CARDBUFFER) * Players.IndexOf(_player),CARDWIDTH,CARDHEIGHT),"Play Card \n" + _player.SelectedCard)){
-						if(_player.SelectedCard != -1){
-							PlayCard(_player, _player.SelectedCard);
-							_player.SelectedCard = -1;
-						}else{
-							Debug.Log("you must select a card to play");
-						}
-					}
 					GUILayout.EndHorizontal();
 				}
+			}*/
+			if(Players[TurnCounter].Hand.Count > 0){
+				GUILayout.BeginHorizontal("");
+				foreach(Card _card in Players[TurnCounter].Hand){
+					if(GUI.Button(new Rect((CARDWIDTH + CARDBUFFER)* Players[TurnCounter].Hand.IndexOf(_card), CARDBUFFER,CARDWIDTH,CARDHEIGHT),_card.FaceValue())){
+						Players[TurnCounter].SelectedCard = Players[TurnCounter].Hand.IndexOf(_card);
+					}
+				}
+				GUILayout.EndHorizontal();
+			}
+			switch(PhaseCounter){
+			case PHASE.DRAW:
+				if(GUI.Button(new Rect((Camera.main.pixelWidth / 2) - CARDWIDTH / 2, Camera.main.pixelHeight / 2,CARDWIDTH,CARDHEIGHT),"Draw")){
+					DrawPhase(Players[TurnCounter]);
+				}
+				break;
+			case PHASE.PLAY:
+				if(GUI.Button(new Rect((Camera.main.pixelWidth / 2) - CARDWIDTH - (CARDBUFFER / 2), Camera.main.pixelHeight / 2,CARDWIDTH,CARDHEIGHT),"Play")){
+					PlayPhase(Players[TurnCounter]);
+				}
+				if(GUI.Button(new Rect((Camera.main.pixelWidth / 2) + (CARDBUFFER / 2), Camera.main.pixelHeight / 2,CARDWIDTH,CARDHEIGHT),"Discard")){
+					DiscardPhase(Players[TurnCounter]);
+				}
+				break;
+			case PHASE.MOVE:
+				if(GUI.Button(new Rect((Camera.main.pixelWidth / 2) - CARDWIDTH / 2, Camera.main.pixelHeight / 2,CARDWIDTH,CARDHEIGHT),"Move")){
+					MovePhase(Players[TurnCounter]);
+				}
+				break;
+			case PHASE.END:
+				if(GUI.Button(new Rect((Camera.main.pixelWidth / 2) - CARDWIDTH / 2, Camera.main.pixelHeight / 2,CARDWIDTH,CARDHEIGHT),"End Turn")){
+					EndPhase(Players[TurnCounter]);
+				}
+				break;
+			}
+			if(LastPlayed != null){
+				GUI.Label(new Rect(0,Camera.main.pixelHeight - CARDHEIGHT, CARDWIDTH, CARDHEIGHT),"Last Played\n" + LastPlayed.FaceValue());
 			}
 		}
 	}
@@ -69,7 +100,7 @@ public class Table : MonoBehaviour {
 			return;
 		}
 		//player draws a card
-		_player.TakeCard(TableDeck.Draw);
+		_player.TakeCard(TableDeck.Draw());
 		//inc the PhaseCounter
 		PhaseCounter = PHASE.PLAY;
 	}
@@ -80,6 +111,7 @@ public class Table : MonoBehaviour {
 		//discard selected card
 		_player.Discard.Add(_player.LastDiscarded);
 		_player.LastDiscarded = _player.Hand[_player.SelectedCard];
+		LastPlayed = _player.Hand[_player.SelectedCard];
 		_player.Hand.RemoveAt(_player.SelectedCard);
 		//inc PhaseCounter
 		PhaseCounter = PHASE.MOVE;
@@ -88,14 +120,24 @@ public class Table : MonoBehaviour {
 		if(TurnCounter != Players.IndexOf(_player) && PhaseCounter != PHASE.PLAY){
 			return;
 		}
+		if(_player.SelectedCard == -1){
+			Debug.Log("you must select a card");
+			return;
+		}
 		//discard selected card
 		_player.Discard.Add(_player.LastDiscarded);
 		_player.LastDiscarded = _player.Hand[_player.SelectedCard];
+		LastPlayed = _player.Hand[_player.SelectedCard];
 		_player.Hand.RemoveAt(_player.SelectedCard);
 		//Player draws or Counts Depending on rule Set
 		switch(RuleSet){
 		case RULESET.CLASSIC:
-			_player.TakeCard(TableDeck.Draw);
+			//_player.TakeCard(TableDeck.Draw());
+			_player.Counting++;
+			if(_player.Counting == 3){
+				//move peg out of player's Home to Come Out space;
+				_player.Counting = 0;
+			}
 			break;
 		}
 		//inc PhaseCounter
@@ -106,12 +148,14 @@ public class Table : MonoBehaviour {
 			return;
 		}
 		//move logic
-		PhaseCounter = PHASE.END
+		PhaseCounter = PHASE.END;
 	}
 	public void EndPhase(Player _player){
 		if(TurnCounter != Players.IndexOf(_player) && PhaseCounter != PHASE.END){
 			return;
 		}
+		_player.SelectedCard = -1;
+		LastPlayed = null;
 		//roll PhaseCounter
 		PhaseCounter = PHASE.DRAW;
 		//roll TurnCounter
@@ -135,7 +179,8 @@ public class Table : MonoBehaviour {
 	public void NewGame(/*default is 2v2*/){
 		TurnCounter = 0;
 		PhaseCounter = PHASE.DRAW;
-		P
+		
+		TableBoard = new Board(4);
 		
 		TableDeck = new Deck(2);
 		TableDeck.Shuffle();
@@ -150,12 +195,12 @@ public class Table : MonoBehaviour {
 		Deal();
 		
 		//print players cards to test
-		foreach(Player _player in Players){
+		/*foreach(Player _player in Players){
 			Debug.Log("" + _player.Name);
 			foreach(Card _card in _player.Hand){
 				Debug.Log(_card.FaceValue());
 			}
-		}
+		}*/
 	}
 	public void Deal(){
 		for(int cardNum = 0; cardNum < HandSize; cardNum++){
@@ -163,6 +208,31 @@ public class Table : MonoBehaviour {
 				//draw a card from the deck and give it to the player
 				_player.TakeCard(TableDeck.Draw());//may need null ref check
 			}
+		}
+	}
+}
+public class Peg{
+	public int team;
+	public int player;
+	public Peg(){
+	}
+}
+public class Board{
+	public Transform[] MainTrackLocations;
+	public Transform[] HomeLocations;
+	public Transform[] CastleLocations;
+	public int[] PegLocations;
+	//0-17 + (18 * player) Main Track
+	//0-4 + (5 * player) Home Track
+	//0-4 + (5 * player) Castle Locations
+	
+	public Board(int _players){
+		MainTrackLocations = new Transform[18 * _players];
+		HomeLocations = new Transform[5 * _players];
+		CastleLocations = new Transform[5 * _players];
+		PegLocations = new int[5 * _players];
+		for(int i = 0; i < PegLocations.Length; i++){
+			PegLocations[i] = i;
 		}
 	}
 }
